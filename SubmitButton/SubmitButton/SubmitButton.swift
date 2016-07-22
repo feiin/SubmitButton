a4555adc9110e71a1391e706cdbc207329239ca7
feiin <feiin@qq.com>
2016-07-22 10:38:43 +0800

    init project

---
+++ SubmitButton/SubmitButton/SubmitButton.swift
@@ -0,0 +1,21 @@
+//
+//  SubmitButton.swift
+//  SubmitButton
+//
+//  Created by yangyin on 16/7/22.
+//  Copyright © 2016年 swiftmi. All rights reserved.
+//
+
+import UIKit
+
+class SubmitButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SubmitButton/SubmitButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SubmitButton
+//
+//  Created by yangyin on 16/7/22.
+//  Copyright © 2016年 swiftmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
